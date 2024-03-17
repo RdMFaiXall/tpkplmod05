82a4b18175caf236fc218d16f85f530db04bd0f9
Faisal Ramadhan <rdmfaisalrj@student.telkomuniversity.ac.id>
2024-03-18 04:26:23 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+    // class bernama HaloGeneric dengan access modifier public
+    public class HaloGeneric
+    {
+        // method generic SapaUser dengan parameter variabel X
+        public void SapaUser<T>(T X)
+        {
+            // menampilkan data yang disimpan pada variabel X
+            Console.WriteLine($"Halo {X}");
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        // memanggil class haloGeneric dan method generic SapaUser
+        HaloGeneric generic = new HaloGeneric();
+        Console.WriteLine("Nama : ");
+        String namaAnda = Console.ReadLine();
+        generic.SapaUser ( namaAnda );
+    }
+}
--- obj/Debug/net8.0/tpmodul5_1302220093.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul5_1302220093.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302220093")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8743f07904e2c248821e1e08734b8635c690c00")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302220093")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302220093")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
